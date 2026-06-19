1715063acbf688239f9a1e68d8d6b5f70b82543e
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-20 02:54:41 +0300

    Update models.py

--- models.py
+++ models.py
@@ -9,3 +9,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(150), unique=True)
     phone = db.Column(db.String(20))
     password = db.Column(db.String(200))
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(20), nullable=False)
+    transaction_code = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, default=100)
+    status = db.Column(db.String(20), default="pending")    
