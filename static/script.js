31ab9c6ad110c64fa9b4805279a47bb25177c560
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-22 10:46:49 +0300

    Refactor password toggle and sidebar functionality

--- static/script.js
+++ static/script.js
@@ -10,3 +10,88 @@ function togglePassword() {
         eyeIcon.src = "/static/images/close-eye.jfif";
     }
 }
+
+window.addEventListener("resize", () => {
+    const sidebar = document.querySelector(".sidebar");
+    const overlay = document.querySelector(".overlay");
+
+    if (window.innerWidth > 768) {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menuBtn = document.querySelector(".dashboard-menu");
+    const cancelBtn = document.querySelector(".cancel");
+    const sidebar = document.querySelector(".sidebar");
+    const overlay = document.querySelector(".overlay");
+
+    menuBtn.addEventListener("click", () => {
+        sidebar.classList.add("active");
+        overlay.classList.add("active");
+    });
+
+    cancelBtn.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+    overlay.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const welcome = document.querySelector(".welcome");
+
+    // show after page loads
+    setTimeout(() => {
+        welcome.classList.add("show");
+    }, 200);
+
+    // hide after 3 seconds (optional)
+    setTimeout(() => {
+        welcome.classList.remove("show");
+    }, 1500);
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const profile = document.getElementById("profileInitial");
+
+    const username = profile.getAttribute("data-username");
+
+    if (username && username.length > 0) {
+        profile.textContent = username.charAt(0).toUpperCase();
+    } else {
+        profile.textContent = "?";
+    }
+
+});
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const profile = document.getElementById("profile");
+
+    const username = profile.getAttribute("data-username");
+
+    if (username && username.length > 0) {
+        profile.textContent = username.charAt(0).toUpperCase();
+    } else {
+        profile.textContent = "?";
+    }
+
+});
