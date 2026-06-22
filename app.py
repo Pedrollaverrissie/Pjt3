857e284ec3aad49b7fd308a372e281abd01f9357
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-22 11:44:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
+import re
 from models import db, User, Payment
 
-app = Flask(__name__)
 app = Flask(__name__)
 app.secret_key = "secret123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
@@ -35,22 +33,35 @@ def home():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
+
+        email = request.form["email"]
+        phone = request.form["phone"]
+
+        # email regex
+        email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+        # kenya phone validation
+        phone_pattern = r"^(07|01)\d{8}$"
+
+        if not re.match(email_pattern, email):
+            return "Invalid email format!"
+
+        if not re.match(phone_pattern, phone):
+            return "Invalid phone number!"
+
         hashed_pw = generate_password_hash(request.form["password"])
 
         new_user = User(
             username=request.form["username"],
-            email=request.form["email"],
-            phone=request.form["phone"],
+            email=email,
+            phone=phone,
             password=hashed_pw
         )
 
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-        except:
-            return "Email already exists!"
+        db.session.add(new_user)
+        db.session.commit()
 
-        return redirect("/payment")
+        return redirect("/login")
 
     return render_template("signup.html")
 
@@ -68,7 +79,7 @@ def login():
 
     return render_template("login.html")
 
-
+#--------------PAYMENT-----------------
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
 
@@ -81,21 +92,30 @@ def payment():
             phone=phone,
             transaction_code=transaction_code,
             amount=100,
-            status='pending'
+            status='approved'
         )
 
         db.session.add(payment)
         db.session.commit()
-
-        return "Payment submitted successfully."
+        
+        return redirect("/login")
 
     return render_template("payment.html")
 
-# ---------------- DASHBOARD ----------------
+# ---------------- DASHBOARD --------s--------
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("dashboard.html", username=current_user.username)
+
+    payments = Payment.query.filter_by(phone=current_user.phone).all()
+
+    total_paid = sum(p.amount for p in payments if p.status == "approved")
+
+    return render_template(
+        "dashboard.html",
+        username=current_user.username,
+        main_amount=total_paid
+    )
 
 # ---------------- LOGOUT ----------------
 @app.route("/logout")
