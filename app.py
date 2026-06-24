d12f043623a5fc540c0081b639b86d901f149fc7
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-24 15:04:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # ------------MAIL CONFIGURATION---------------
-app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
+app.config['MAIL_PORT'] = 465
+
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 
 app.config["MAIL_DEFAULT_SENDER"] = "petersongitonga02@gmail.com"
 
-app.config['MAIL_DEBUG'] = True
+app.config['MAIL_DEBUG'] = False
 app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_MAX_EMAILS'] = None
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
