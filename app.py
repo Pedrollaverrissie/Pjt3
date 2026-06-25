9383f6ce1d14f8b5932578f1276a3cbc63248d46
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-25 09:24:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,21 @@ load_dotenv()
 
 app = Flask(__name__)
 
+database_url = os.getenv("DATABASE_URL")
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+db = SQLAlchemy(app)
+
 app.secret_key = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 INTASEND_PUBLISHABLE_KEY = os.getenv("INTASEND_PUBLISHABLE_KEY")
