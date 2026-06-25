04a45b34313c429637ec5622a30042e4dcc99f2d
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-25 09:44:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -506,7 +506,9 @@ def db_test():
     Pending Users: {PendingUser.query.count()}<br>
     Payments: {Payment.query.count()}
     """
-
+@app.route("/db-url")
+def db_url():
+    return app.config["SQLALCHEMY_DATABASE_URI"]
 
 if __name__ == "__main__":
     app.run(debug=True)
