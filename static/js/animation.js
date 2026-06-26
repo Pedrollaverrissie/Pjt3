2af0ed4276fa4ee96e6ff08ba518ae2742afe5e3
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-26 23:52:34 +0300

    Update animation.js

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,1 +1,12 @@
 
+function movePlane(x, y) {
+    const plane = document.getElementById("plane");
+
+    plane.style.left = x + "px";
+    plane.style.top = y + "px";
+}
+
+function updateMultiplier(value) {
+    document.getElementById("multiplier").innerHTML =
+        value.toFixed(2) + "x";
+}
