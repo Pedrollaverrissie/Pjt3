4161b528bc4ee994de6878ae724de8845aea0891
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-27 01:27:36 +0300

    Update rounds.js

--- static/js/rounds.js
+++ static/js/rounds.js
@@ -1,2 +1,37 @@
-function startRound(){}
-function crashRound(){}
+const gameStatus = document.getElementById("gameStatus");
+
+// Generates a random crash point (temporary)
+// Later this will come from Flask
+function generateCrashPoint() {
+
+    return Number((Math.random() * 8 + 2).toFixed(2));
+
+}
+
+function startRound() {
+
+    gameStatus.textContent = "Flying...";
+
+    const crashPoint = generateCrashPoint();
+
+    animateFlight(crashPoint);
+
+}
+
+function crashRound(crashPoint) {
+
+    multiplier.textContent = "💥 " + crashPoint.toFixed(2) + "x";
+
+    gameStatus.textContent = "CRASHED";
+
+    addHistory(crashPoint);
+
+    setTimeout(() => {
+
+        gameStatus.textContent = "Next round...";
+
+        setTimeout(startRound, 2000);
+
+    }, 2000);
+
+}
