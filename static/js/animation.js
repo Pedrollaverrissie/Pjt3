f26408ea9d6936da4a8eb516ed5cc5319ec7e604
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-27 00:14:42 +0300

    Update animation.js

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,12 +1,27 @@
-
 function movePlane(x, y) {
     const plane = document.getElementById("plane");
-
     plane.style.left = x + "px";
     plane.style.top = y + "px";
 }
 
 function updateMultiplier(value) {
-    document.getElementById("multiplier").innerHTML =
+    document.getElementById("multiplier").textContent =
         value.toFixed(2) + "x";
+}
+
+function drawGraph(progress) {
+
+    const path = document.getElementById("flightPath");
+
+    const x = 50 + progress;
+
+    // Curved flight path
+    const y = 400 - Math.pow(progress / 18, 1.5);
+
+    path.setAttribute(
+        "d",
+        `M50 400 Q${50 + progress / 2} ${400 - progress / 4} ${x} ${y}`
+    );
+
+    movePlane(x, y);
 }
