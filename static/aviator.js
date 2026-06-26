7df93278bc4d0a128c5c1b336de176132300fa08
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-26 21:25:15 +0300

    Update aviator.js

--- static/aviator.js
+++ static/aviator.js
@@ -1,10 +1,43 @@
+const canvas = document.getElementById("graph");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
 let multiplier = 1;
 
-setInterval(function(){
+let x = 40;
 
-    multiplier += 0.01;
+let y = canvas.height - 50;
+
+ctx.beginPath();
+
+ctx.moveTo(x,y);
+
+setInterval(()=>{
+
+    multiplier += 0.02;
 
     document.getElementById("multiplier").innerHTML =
-        multiplier.toFixed(2) + "x";
+        multiplier.toFixed(2)+"x";
 
-},100);
+    x += 2;
+
+    y -= multiplier * 0.15;
+
+    ctx.lineWidth = 4;
+
+    ctx.strokeStyle="#00ff88";
+
+    ctx.lineTo(x,y);
+
+    ctx.stroke();
+
+    const rocket = document.querySelector(".rocket");
+
+    rocket.style.left = x+"px";
+
+    rocket.style.top = (y-20)+"px";
+
+},40);
