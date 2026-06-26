b8aab2e8c9ba8c2d6087ca5cc5c3d2081ea6b119
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-26 23:40:44 +0300

    Update aviator.js

--- static/js/aviator.js
+++ static/js/aviator.js
@@ -1,63 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
     const multiplier = document.getElementById("multiplier");
     const gameStatus = document.getElementById("gameStatus");
     const plane = document.getElementById("plane");
+    const path = document.getElementById("flightPath");
 
-    console.log(multiplier);
-    console.log(gameStatus);
-    console.log(plane);
+    function startRound(){
 
-    let current = 1.00;
-    let animation = null;
+        let current = 1;
+        let progress = 0;
 
-    function startRound() {
+        const crashPoint = Number((Math.random()*8+2).toFixed(2));
 
-        current = 1.00;
+        multiplier.innerHTML="1.00x";
+        gameStatus.innerHTML="Flying...";
 
-        multiplier.innerHTML = current.toFixed(2) + "x";
-        gameStatus.innerHTML = "Flying...";
+        const timer = setInterval(()=>{
 
-        plane.style.left = "60px";
-        plane.style.bottom = "70px";
+            current +=0.02;
 
-        const crashPoint = Number((Math.random() * 9 + 1).toFixed(2));
+            multiplier.innerHTML=current.toFixed(2)+"x";
 
-        let left = 60;
-        let bottom = 70;
+            progress +=4;
 
-        animation = setInterval(() => {
+            const x = 50 + progress;
 
-            current += 0.02;
+            const y = 400 - Math.pow(progress/22,1.5);
 
-            multiplier.innerHTML = current.toFixed(2) + "x";
+            plane.style.left=x+"px";
+            plane.style.top=y+"px";
 
-            left += 3;
-            bottom += 1.8;
+            path.setAttribute(
+                "d",
+                `M50 400 Q${x/2} ${400-(progress/5)} ${x} ${y}`
+            );
 
-            plane.style.left = left + "px";
-            plane.style.bottom = bottom + "px";
+            if(current>=crashPoint){
 
-            if (current >= crashPoint) {
+                clearInterval(timer);
 
-                clearInterval(animation);
+                multiplier.innerHTML="💥 "+crashPoint.toFixed(2)+"x";
 
-                multiplier.innerHTML = "💥 " + crashPoint.toFixed(2) + "x";
-                gameStatus.innerHTML = "CRASHED";
+                gameStatus.innerHTML="CRASHED";
 
-                setTimeout(() => {
+                setTimeout(startRound,3000);
 
-                    gameStatus.innerHTML = "Next round starting...";
-
-                    setTimeout(startRound, 3000);
-
-                }, 2000);
             }
 
-        }, 40);
+        },40);
 
     }
 
-    setTimeout(startRound, 3000);
+    setTimeout(startRound,2000);
 
 });
