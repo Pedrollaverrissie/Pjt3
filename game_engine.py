4fdc313f2ab3ba84da16d4e065dff154153b1e5a
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 08:05:57 +0300

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,33 +1,12 @@
-
-import random
-
-
 class AviatorGame:
 
     def __init__(self):
 
-        self.multiplier = 1.00
         self.status = "waiting"
+        self.multiplier = 1.00
+        self.current_crash = 0
         self.round_id = 1
-
-        self.current_crash = self.generate_crash()
-        self.next_crash = self.generate_crash()
-
         self.countdown = 5
 
-    def generate_crash(self):
-
-        return round(random.uniform(1.10, 12.00), 2)
 
 game = AviatorGame()
-
-
-
-
-
-
-
-
-
-
-
