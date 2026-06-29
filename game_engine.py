6ea8228c83bdd2a1edef2ce9c67389fd48f38aff
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 07:59:22 +0300

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,5 +1,4 @@
-import threading
-import time
+
 import random
 
 
@@ -20,49 +19,9 @@ class AviatorGame:
 
         return round(random.uniform(1.10, 12.00), 2)
 
-    def game_loop(self):
-
-        while True:
-
-            # WAITING
-            self.status = "waiting"
-
-            for i in range(5,0,-1):
-
-                self.countdown = i
-
-                time.sleep(1)
-
-            # START ROUND
-            self.status = "flying"
-
-            self.multiplier = 1.00
-
-            while self.multiplier < self.current_crash:
-
-                self.multiplier += 0.02
-
-                time.sleep(0.05)
-
-            # CRASH
-            self.status = "crashed"
-
-            time.sleep(2)
-
-            self.round_id += 1
-
-            self.current_crash = self.next_crash
-
-            self.next_crash = self.generate_crash()
-
-
 game = AviatorGame()
 
-thread = threading.Thread(target=game.game_loop)
 
-thread.daemon = True
-
-thread.start()
 
 
 
