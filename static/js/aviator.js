f819f0d54f56ba219d36f742a869983c9a0b6ed3
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 07:44:02 +0300

    Update aviator.js

--- static/js/aviator.js
+++ static/js/aviator.js
@@ -1,5 +1,58 @@
+let started = false;
+
+async function updateGame() {
+
+    try {
+
+        const response = await fetch("/game-state");
+        const game = await response.json();
+
+        // Waiting
+        if (game.status === "waiting") {
+
+            started = false;
+
+            gameStatus.textContent =
+                "Next round in " + game.countdown + "s";
+
+        }
+
+        // Flying
+        else if (game.status === "flying") {
+
+            if (!started) {
+
+                started = true;
+
+                animateFlight(game.current_crash);
+
+            }
+
+        }
+
+        // Crashed
+        else if (game.status === "crashed") {
+
+            started = false;
+
+            gameStatus.textContent = "💥 CRASHED";
+
+        }
+
+    }
+
+    catch (err) {
+
+        console.log(err);
+
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("🚀 Supernova Aviator Loaded");
+    console.log("🚀 Supernova Connected");
 
-});
+    setInterval(updateGame, 200);
+
+});                                                                                                         
