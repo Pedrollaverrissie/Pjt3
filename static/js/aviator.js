4c939379331260d8b2a8c2686b8199758bf59fe0
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 07:17:12 +0300

    Update aviator.js

--- static/js/aviator.js
+++ static/js/aviator.js
@@ -2,7 +2,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("🚀 Supernova Aviator Loaded");
 
-    // Start first round after 3 seconds
-    setTimeout(startRound, 3000);
-
 });
