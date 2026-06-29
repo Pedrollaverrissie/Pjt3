cca4f7a2027d8cb7b31cbb706b7ab5f3e11b5a7a
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 22:32:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ if database_url.startswith("postgres://"):
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
 
 app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=30)
 
