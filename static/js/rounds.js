5646bcfeb7d2481b135107bcb8ae7676df5e0b0a
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-29 08:11:54 +0300

    Update rounds.js

--- static/js/rounds.js
+++ static/js/rounds.js
@@ -1,52 +1,23 @@
 const gameStatus = document.getElementById("gameStatus");
 
-async function startRound() {
+setInterval(async () => {
 
-    // Ask the Flask server for the current game
     const response = await fetch("/game-state");
 
     const game = await response.json();
 
-    // If waiting
-    if (game.status === "waiting") {
+    gameStatus.textContent = game.status;
 
-        gameStatus.textContent =
-            "Next round in " + game.countdown + "s";
-
-        setTimeout(startRound, 1000);
-
-        return;
-    }
-
-    // If flying
     if (game.status === "flying") {
 
-        gameStatus.textContent = "Flying...";
+        drawFlight(game.multiplier);
 
-        animateFlight(game.current_crash);
-
-        return;
     }
 
-    // If crashed
     if (game.status === "crashed") {
 
-        crashRound(game.current_crash);
-
-        setTimeout(startRound,1000);
+        drawCrash(game.multiplier);
 
     }
 
-}
-
-function crashRound(crashPoint){
-
-    multiplier.textContent =
-        "💥 " + crashPoint.toFixed(2) + "x";
-
-    gameStatus.textContent =
-        "CRASHED";
-
-    addHistory(crashPoint);
-
-}
+}, 100);
