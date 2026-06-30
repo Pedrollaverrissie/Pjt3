113ef6f82be5dc949a25c637bb61cf137d59941f
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-30 21:53:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from game_engine import game
+from game_engine import game
 from flask import Flask, render_template, request, redirect,jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
