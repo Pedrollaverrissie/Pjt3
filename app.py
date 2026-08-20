aae2fe11564866c7c4df57d288b14dffa08cdb17
Pedrollaverrissie <petersongitonga42@gmail.com>
2026-08-20 15:09:48 +0300

    Update project changes

--- app.py
+++ app.py
@@ -2291,7 +2291,7 @@ def recharge():
                 renewal_shortfall=0,
                 purpose=purpose,
                 first_recharge=True,
-                recharge_error="Your Activation Amount is Ksh.200."
+                recharge_error="Your Activation Amount is Ksh. 200"
             )
 
         if phone.startswith("0"):
